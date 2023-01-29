c12ee146f868626cc2d8fbb383ab5e50f3899f8d
Eunyu Seo <87485508+EUNYUSEO@users.noreply.github.com>
2023-01-30 00:13:51 +0900

    Revert "2주차 문제풀이"

--- src/2주차/TubleToObject/유.ts
+++
@@ -1,7 +0,0 @@
-const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
-
-type TupleToObject<T extends readonly string[]> = {
-  [k in T[number]]: k;
-};
-
-type result = TupleToObject<typeof tuple>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
--- src/3주차/Exclude/유.ts
+++
@@ -1,5 +0,0 @@
-// T에서 U에 할당할 수 있는 타입을 제외하는 내장 제네릭 Exclude<T, U>를 이를 사용하지 않고 구현하세요.
-
-type MyExclude<T, U> = T extends U ? never : T;
-
-type Result = MyExclude<"a" | "b" | "c", "a">; // 'b' | 'c'
