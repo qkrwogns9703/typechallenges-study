1de802a0c67ad3655aeb8dedbe082ba4dd73af8c
qkrwogns9703 <jaehunb350@gmail.com>
2023-03-03 16:08:35 +0900

    feat: 8주차 문제풀이

--- src/8주차/Reedonly/줄리안.ts
+++ src/8주차/Reedonly/줄리안.ts
@@ -1,5 +1,3 @@
-type MyReadonly2<T, K extends keyof T = keyof T> = {
-  [key in keyof T as key extends K ? never : key]: T[key];
-} & {
+type MyReadonly2<T, K extends keyof T = keyof T> = MyOmit<T, K> & {
   readonly [key in keyof T as key extends K ? key : never]: T[key];
 };
